9842fae80938d2269c66cb3185c95d91212b54f0
Prem1kr <prem78334@gmail.com>
2026-06-05 21:53:38 +0530

    isOnline logic added

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,14 +1,20 @@
+import authModel from "../models/authModel.js";
 
 export const initializeSocket = (io) => {
     io.on("connection", (socket) => {
         console.log("user connected", socket.id);
 
-        socket.on("join", (userId) => {
+        socket.on("join", async (userId) => {
+            await authModel.findByIdAndUpdate(userId, { isOnline: true });
             socket.join(userId);
-            console.log(`${userId} joined `);
+            console.log(`${userId} joined`);
+
         });
 
         socket.on("disconnect", () => {
+            if (socket.userId) {
+                await User.findByIdAndUpdate(socket.userId, { isOnline: false, lastActive: new Date() });
+            }
             console.log("user disconnected", socket.id);
         });
 
